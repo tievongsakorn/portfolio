2deaa0203b71b26f15c28dcd2a157fcc0ff87265
Vk. <vongsakorntie@gmail.com>
2024-06-25 13:05:12 +0700

    fix vcard

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,6 @@ Route::get('/about', [AppController::class, 'about']);
 
 Route::get('/contact', [AppController::class, 'contact']);
 Route::get('/vcard', function () {
-    return Storage::download('contact/vongsakoen_vCard.vcf');
+    return Storage::download('app/contact/vongsakoen_vCard.vcf');
 });
 
