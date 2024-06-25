d935381119dc8585b654dfeecce7d427c80311e9
Vk. <vongsakorntie@gmail.com>
2024-06-25 08:16:08 +0700

    dell follow on footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -5,13 +5,10 @@
                 <a class="footer_mail text-decoration-none" href="mailto:vongsakorntie@gmail.com">Email : vongsakorntie@gmail.com</a>
             </div>
         </div>
-        <div class="col-12 col-md-auto d-flex align-items-center justify-content-center justify-content-md-start">
-            follow me on:
-        </div>
         <div class="col-12 col-md-auto d-flex align-items-center justify-content-end justify-content-center justify-content-md-start">
             <div class="copy_right p-2 ">
                 © 2024 By Vongsakorn Ketsuritong <br>
-                Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                Run on Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
             </div>
         </div>
     </div>
